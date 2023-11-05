540e8ad76051d13b380af24f4e2578c82d83ffec
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2023-11-05 12:34:28 +0300

    up

--- 28-StockAPI_3/backend/src/routes/index.js
+++ 28-StockAPI_3/backend/src/routes/index.js
@@ -10,24 +10,27 @@ const router = require('express').Router()
 
 // auth:
 router.use('/account/auth', require('./auth'))
+// call user.create for /account/register:
+const { create: userCreate } = require('../controllers/user')
+router.post('/account/register', userCreate)
+
 // user:
 router.use('/users', require('./user'))
 // token:
 router.use('/tokens', require('./token'))
 
-
 // brand:
 router.use('/stock/brands', require('./brand'))
 // category:
-router.use("/stock/categories", require("./category"));
+router.use('/stock/categories', require('./category'))
 // firm:
-router.use("/stock/firms", require("./firm"));
+router.use('/stock/firms', require('./firm'))
 // product:
-router.use("/stock/products", require("./product"));
+router.use('/stock/products', require('./product'))
 // purchase:
-router.use("/stock/purchases", require("./purchase"));
+router.use('/stock/purchases', require('./purchase'))
 // sale:
-router.use("/stock/sales", require("./sale"));
+router.use('/stock/sales', require('./sale'))
 
 // document:
 router.use('/documents', require('./document'))
